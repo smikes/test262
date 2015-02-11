93a5efd1064da69a7c950db89ecb7bf7e498bc3a
Mike Pennisi <mike@mikepennisi.com>
2015-02-11 13:19:08 -0500

    Move frontmatter after license block

--- test/annexB/B.2.3.4.js
+++ test/annexB/B.2.3.4.js
@@ -1,12 +1,13 @@
-/*---
- includes: [v8-mjsunit.js]
- ---*/
 // Copyright 2014 the V8 project authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 // Tests taken from:
 // http://mathias.html5.org/tests/javascript/string/
+
+/*---
+ includes: [v8-mjsunit.js]
+ ---*/
 
 assertEquals('_'.blink(), '<blink>_</blink>');
 assertEquals('<'.blink(), '<blink><</blink>');
